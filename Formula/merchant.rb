bf1356b04ab916793a4857f2479e8d2af486691d
Sam Roberts <samgqroberts@gmail.com>
2024-09-10 17:59:13 -0400

    chore: update to 0.2.0

--- Formula/merchant.rb
+++ Formula/merchant.rb
@@ -1,9 +1,15 @@
 class Merchant < Formula
   desc "Terminal UI game"
   homepage "https://github.com/samgqroberts/merchant"
-  url "https://github.com/samgqroberts/merchant/releases/download/v0.1.0/merchant-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "277332ebb5fb7fb0c80db2e25d1abe0ce39b4015407eff3294d4d145afac41d7"
-  version "0.1.0"
+  on_arm do
+    url "https://github.com/samgqroberts/merchant/releases/download/v0.2.0/merchant-0.2.0-aarch64-apple-darwin.tar.gz"
+    sha256 "e1d80ee8a84c4343d1df3eb9e8ff7801651b50cf3790326cd30172e69ce6287d"
+  end
+  on_intel do
+    url  "https://github.com/samgqroberts/merchant/releases/download/v0.2.0/merchant-0.2.0-x86_64-apple-darwin.tar.gz"
+    sha256 "b797840894cf41d0038ffe9410a7ee46ce069bd49c891f4f9661b663a3b24406"
+  end
+  version "0.2.0"
 
   def install
     bin.install "merchant"
